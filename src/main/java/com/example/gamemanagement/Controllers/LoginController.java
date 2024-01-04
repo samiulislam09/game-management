622669a4ed8106b6833b78f1b372d28cdbfeb503
samiulislam09 <samiulislamsawon09@gmail.com>
2024-01-04 22:41:34 +0600

    added login page

--- src/main/java/com/example/gamemanagement/Controllers/LoginController.java
+++ src/main/java/com/example/gamemanagement/Controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.example.gamemanagement.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public TextField username_selector;
+    public TextField password_selector;
+    public Button login_btn;
 }
